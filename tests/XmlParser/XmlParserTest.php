3278a1a55e6348c77bb111ee9db2cc370b7a0232
mazor <mazor@student.21-school.ru>
2022-07-15 22:28:09 +0400

    refactored XmlParserTest.php

--- tests/XmlParser/XmlParserTest.php
+++ tests/XmlParser/XmlParserTest.php
@@ -2,30 +2,27 @@
 
 namespace Parser;
 
-use phpDocumentor\Reflection\Types\This;
 use PHPUnit\Framework\TestCase;
 
 class XmlParserTest extends TestCase {
     private string $filename = '/var/www/html/training.xml';
     private string $expected = 'Lafferty';
 
-    public function testDom() {
-        $parser = new DomParser($this->filename);
+    /**
+     * @dataProvider classProvider
+     */
+    public function testParser($className) {
+        $fullClassName = __NAMESPACE__ . '\\' . $className;
+        $parser = new $fullClassName($this->filename);
         $this->assertSame($this->expected, $parser->findLastname());
     }
 
-    public function testDomXpath() {
-        $parser = new DomXpathParser($this->filename);
-        $this->assertSame($this->expected, $parser->findLastname());
-    }
-
-    public function testSimpleXml() {
-        $parser = new SimplexmlParser($this->filename);
-        $this->assertSame($this->expected, $parser->findLastname());
-    }
-
-    public function testSimpleXpath() {
-        $parser = new SimplexmlXpathParser($this->filename);
-        $this->assertSame($this->expected, $parser->findLastname());
+    public function classProvider() {
+        return [
+            ['DomParser'],
+            ['DomXpathParser'],
+            ['SimplexmlParser'],
+            ['SimplexmlXpathParser']
+        ];
     }
 }
