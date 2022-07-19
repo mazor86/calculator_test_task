6f656cc6e43267980c1d822bde27e64d5bdf70df
mazor86 <kornienkoroman86@gmail.com>
2022-07-19 23:41:57 +0400

    fixed XmlReaderParser.php

--- src/Parser/XmlReaderParser.php
+++ src/Parser/XmlReaderParser.php
@@ -5,17 +5,19 @@ namespace Parser;
 use XMLReader;
 
 class XmlReaderParser extends Parser {
+    protected bool $flag = false;
 
     public function findLastname() {
-        while ($this->xml->name != 'Attribute'
-            or $this->xml->getAttribute('Name') !== 'Last name'
-        ) {
-            $this->xml->read();
+        while ($this->xml->read()) {
+            if ($this->xml->name == 'Attribute'
+                && $this->xml->getAttribute('Name') == 'Last name') {
+                $this->flag = true;
+            }
+            if ($this->flag && $this->xml->nodeType == XMLReader::TEXT) {
+                return $this->xml->value;
+            }
         }
-        while ($this->xml->nodeType != XMLReader::TEXT) {
-            $this->xml->read();
-        }
-        return $this->xml->value;
+        return null;
     }
 
     protected function setXml() {
